0f4a9baa513c8723243ae8ec1d1e4edc8ba4c0a7
rocksword <rocksword@163.com>
2015-04-16 17:12:17 +0800

    Dao

--- src/main/java/com/an/antry/spring/appcontext/StaticDataWeatherDAOImpl.java
+++ src/main/java/com/an/antry/spring/appcontext/StaticDataWeatherDAOImpl.java
@@ -3,18 +3,15 @@ package com.an.antry.spring.appcontext;
 import java.util.Date;
 
 public class StaticDataWeatherDAOImpl implements WeatherDAO {
-    @Override
     public WeatherData find(Date date) {
         WeatherData wd = new WeatherData(11.11111);
         return wd;
     }
 
-    @Override
     public WeatherData save(Date date) {
         return null;
     }
 
-    @Override
     public WeatherData update(Date date) {
         return null;
     }
